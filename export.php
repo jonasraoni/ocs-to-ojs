f1dec90c2ef215630ec9fab33be0c05be1c26fe4
Jonas Raoni Soares da Silva <github@raoni.org>
2023-04-20 16:56:51 +0300

    Added export script

---
+++ export.php
@@ -0,0 +1,292 @@
+<?php
+
+class Exporter {
+    private $runningPath;
+    private $ocsPath;
+    private $outputPath;
+    private $force;
+    private $conferences;
+
+    public static function run($argv)
+    {
+        $options = getopt('i:o:p:f');
+        if (empty($options['i']) || empty($options['o'])) {
+            exit('Usage: export.php -i PATH_TO_OCS_INSTALLATION -o OUTPUT_PATH [-f] [conference_path1 [conferenceN...]]');
+        }
+
+        $conferences = array_filter(array_slice($argv, count($options) + count(array_filter($options, 'is_string')) + 1), 'strlen');
+        new static($options['i'], $options['o'], $conferences, isset($options['f']));
+    }
+
+    private function __construct($ocsPath, $outputPath, $conferences, $force)
+    {
+        ini_set('memory_limit', -1);
+        set_time_limit(0);
+        $this->runningPath = realpath(getcwd());
+        $this->ocsPath = realpath($ocsPath);
+        $this->outputPath = $outputPath;
+        $this->force = $force;
+        $this->conferences = $conferences;
+        $this->bootOcs();
+        $this->checkOcsVersion();
+        $exception = null;
+        try {
+            $this->createOutputPath();
+            $this->exportMetadata();
+            $this->exportPapers();
+        } catch (Exception $exception) {
+        } finally {
+            $this->log($exception ? "Export failed with {$exception}" : 'Export finished with success');
+            echo chr(7);
+        }
+    }
+
+    private function createOutputPath()
+    {
+        $currentDirectory = getcwd();
+        chdir($this->runningPath);
+        if (is_dir($this->outputPath)) {
+            if (!$this->force) {
+                $this->outputPath = realpath($this->outputPath);
+                throw new Exception("Output path \"{$this->outputPath}\" already exists, quitting for security. Ensure the output path doesn't exist or use the -f option");
+            }
+        } else {
+            static::log("Creating output path {$this->outputPath}");
+            if (!mkdir($this->outputPath, 0777, true)) {
+                throw new Exception('Failed to create output directory');
+            }
+        }
+        $this->outputPath = realpath($this->outputPath);
+        static::log("Output path created");
+        chdir($currentDirectory);
+    }
+
+    private function bootOcs()
+    {
+        if (!is_file($this->ocsPath . "/config.inc.php")) {
+            throw new Exception("The path \"{$this->ocsPath}\" doesn't seem to be a valid OCS installation, the config.inc.php file wasn't found.");
+        }
+
+        $this->log('Booting OCS');
+        define('INDEX_FILE_LOCATION', "{$this->ocsPath}/index.php");
+        chdir(dirname(INDEX_FILE_LOCATION));
+        require_once 'lib/pkp/includes/bootstrap.inc.php';
+        Application::getRequest();
+        $this->log('Booting complete');
+    }
+
+    private static function log($message)
+    {
+        echo "{$message}\n";
+    }
+
+    private function checkOcsVersion() {
+        $this->log('Checking OCS version');
+        $version = $this->readAll(
+            "SELECT v.major, v.minor, v.revision, v.build
+            FROM versions v
+            WHERE
+                v.current = 1
+                AND v.product_type = 'core'
+                AND v.product = 'ocs2'"
+        );
+        $requiredVersion = '2.3.6.0';
+        $version = implode('.', (array) reset($version));
+        if ($version !== $requiredVersion) {
+            throw new Exception("This script is compatible only with OCS {$requiredVersion}, your OCS version {$version} must be downgraded/upgraded");
+        }
+        $this->log('OCS version checked');
+    }
+
+    private function exportMetadata()
+    {
+        $path = $this->outputPath . "/metadata.json";
+        $this->log("Exporting OCS metadata to {$path}");
+        if (!file_put_contents($path, json_encode($this->getConferences(), JSON_PRETTY_PRINT))) {
+            throw new Exception('Failed to generate the OCS metadata');
+        }
+        $this->log('Metadata exported');
+    }
+
+    private function exportPapers()
+    {
+        $this->log('Exporting OCS papers');
+
+        /** @var ConferenceDAO */
+        $conferenceDao = DAORegistry::getDAO('ConferenceDAO');
+        /** @var SchedConfDAO */
+        $schedConfDao = DAORegistry::getDAO('SchedConfDAO');
+        /** @var TrackDAO */
+        $trackDao = DAORegistry::getDAO('TrackDAO');
+        /** @var PublishedPaperDAO */
+        $publishedPaperDao = DAORegistry::getDAO('PublishedPaperDAO');
+
+        mkdir("{$this->outputPath}/papers", 0777, true);
+        $locale = AppLocale::getPrimaryLocale();
+        $conferences = count($this->conferences)
+            ? array_map(
+                function ($conference) use ($conferenceDao) {
+                    return $conferenceDao->getConferenceByPath($conference);
+                }, $this->conferences
+            )
+            : $conferenceDao->getConferences()->toArray();
+        foreach ($conferences as $conference) {
+            $this->log('Processing conference ' . $conference->getTitle($locale));
+            foreach ($schedConfDao->getSchedConfsByConferenceId($conference->getId())->toArray() as $schedConf) {
+                $this->log('Processing scheduled conference ' . $schedConf->getTitle($locale));
+                $tracks = $trackDao->getSchedConfTracks($schedConf->getId())->toAssociativeArray('id');
+                foreach ($publishedPaperDao->getPublishedPapersBySchedConfId($schedConf->getId())->toArray() as $paper) {
+                    $this->log('Processing paper ID ' . $paper->getPaperId());
+                    $track = isset($tracks[$paper->getTrackId()]) ? $tracks[$paper->getTrackId()] : null;
+                    require_once 'generators/stable-3_2_1.php';
+                    $filename = "{$this->outputPath}/papers/{$conference->getId()}-{$schedConf->getId()}-{$paper->getTrackId()}-{$paper->getId()}.xml";
+                    try {
+                        NativeXmlGenerator::renderPaper($filename, $conference, $schedConf, $track, $paper);
+                    } catch(Throwable $e) {
+                        $this->log("Failed to generate paper with {$e}");
+                        continue;
+                    }
+                }
+            }
+        }
+
+        $this->log('Papers exported');
+    }
+
+    private function getConferences()
+    {
+        $conferenceParams = substr(str_repeat('?,', count($this->conferences)), 0, -1);
+        list($filter, $params) = count($this->conferences) ? [sprintf('WHERE c.path IN (%s)', $conferenceParams), $this->conferences] : ['', []];
+        
+        $conferences = $this->readAll(
+            "SELECT c.conference_id, c.path, c.primary_locale, cs.setting_name, cs.setting_value, cs.locale
+            FROM conferences c
+            LEFT JOIN conference_settings cs ON cs.conference_id = c.conference_id
+            $filter",
+            $params
+        );
+
+        $conferences = array_values(
+            array_reduce($conferences, function($conferences, $conference) {
+                $conferences[$conference->conference_id] = isset($conferences[$conference->conference_id])
+                    ? $conferences[$conference->conference_id]
+                    : [
+                        'id' => $conference->conference_id,
+                        'path' => $conference->path,
+                        'primaryLocale' => $conference->primary_locale,
+                    ];
+                if ($conference->locale) {
+                    $conferences[$conference->conference_id][$conference->setting_name][$conference->locale] = $conference->setting_value;
+                } else {
+                    $conferences[$conference->conference_id][$conference->setting_name] = $conference->setting_value;
+                }
+                return $conferences;
+            })
+        );
+
+        if (count($this->conferences) && count($conferences) !== count($this->conferences)) {
+            $notFound = array_diff(
+                $this->conferences,
+                array_map(
+                    function ($conference) {
+                        return $conference->path;
+                    },
+                    $conferences
+                )
+            );
+            throw new Exception('The following conferences were not found:' . implode(', ', $notFound));
+        }
+
+        foreach ($conferences as &$conference) {
+            $conference['scheduledConferences'] = $this->getScheduledConferences($conference['path']);
+
+        }
+
+        return $conferences;
+    }
+
+    private function getScheduledConferences($conference)
+    {
+        $scheduledConferences = $this->readAll(
+            "SELECT sc.sched_conf_id, sc.path, sc.seq, sc.start_date, sc.end_date, scs.setting_name, scs.setting_value, scs.locale
+            FROM sched_confs sc
+            INNER JOIN conferences c ON c.conference_id = sc.conference_id AND c.path = ?
+            LEFT JOIN sched_conf_settings scs ON scs.sched_conf_id = sc.sched_conf_id",
+            [$conference]
+        );
+
+        $scheduledConferences = array_values(
+            array_reduce($scheduledConferences, function($scheduledConferences, $scheduledConference) {
+                $scheduledConferences[$scheduledConference->sched_conf_id] = isset($scheduledConferences[$scheduledConference->sched_conf_id])
+                    ? $scheduledConferences[$scheduledConference->sched_conf_id]
+                    : [
+                        'id' => $scheduledConference->sched_conf_id,
+                        'path' => $scheduledConference->path,
+                        'seq' => $scheduledConference->seq,
+                        'startDate' => $scheduledConference->start_date,
+                        'endDate' =>  $scheduledConference->end_date
+                    ];
+                if ($scheduledConference->locale) {
+                    $scheduledConferences[$scheduledConference->sched_conf_id][$scheduledConference->setting_name][$scheduledConference->locale] = $scheduledConference->setting_value;
+                } else {
+                    $scheduledConferences[$scheduledConference->sched_conf_id][$scheduledConference->setting_name] = $scheduledConference->setting_value;
+                }
+                return $scheduledConferences;
+            })
+        );
+
+        foreach ($scheduledConferences as &$scheduledConference) {
+            $scheduledConference['tracks'] = $this->getTracks($conference, $scheduledConference['path']);
+        }
+
+        return $scheduledConferences;
+    }
+
+    private function getTracks($conference, $scheduledConference)
+    {
+        $tracks = $this->readAll(
+            "SELECT t.track_id, t.seq, ts.setting_name, ts.setting_value, ts.locale
+            FROM tracks t
+            INNER JOIN sched_confs sc ON sc.sched_conf_id = t.sched_conf_id AND sc.path = ?
+            INNER JOIN conferences c ON c.conference_id = sc.conference_id AND c.path = ?
+            LEFT JOIN track_settings ts ON ts.track_id = t.track_id",
+            [$scheduledConference, $conference]
+        );
+
+        return array_values(
+            array_reduce($tracks, function($tracks, $track) {
+                $tracks[$track->track_id] = isset($tracks[$track->track_id])
+                    ? $tracks[$track->track_id]
+                    : [
+                        'id' => $track->track_id,
+                        'seq' => $track->seq
+                    ];
+                if ($track->locale) {
+                    $tracks[$track->track_id][$track->setting_name][$track->locale] = $track->setting_value;
+                } else {
+                    $tracks[$track->track_id][$track->setting_name] = $track->setting_value;
+                }
+                return $tracks;
+            })
+        );
+    }
+
+    private function readAll($query, $params = [])
+    {
+        $dao = new DAO();
+        $rs = $dao->retrieve($query, $params);
+        $data = [];
+        while (!$rs->EOF) {
+            $row = (object) $rs->GetRowAssoc(0);
+            foreach ($row as &$value) {
+                $value = iconv('UTF-8', 'UTF-8//TRANSLIT', $value);
+            }
+            $data[] = $row;
+            $rs->MoveNext();
+        }
+        $rs->Close();
+        return $data;
+    }
+}
+
+Exporter::run($argv);
