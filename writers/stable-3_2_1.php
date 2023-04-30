332702ef4844c645e7d471917582b9430ab70593
Jonas Raoni Soares da Silva <github@raoni.org>
2023-05-01 01:28:14 +0300

    Organized methods

--- writers/stable-3_2_1.php
+++ writers/stable-3_2_1.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file writers/stable-3_2_1.php
  *
@@ -20,40 +21,18 @@ class Stable321Writer extends BaseXmlWriter {
      */
     public function process()
     {
-        // Article
-        $articleNode = $this->document->appendChild($this->document->createElementNS('http://pkp.sfu.ca', 'article'));
-        $this->setAttributes($articleNode, [
-            'xsi:schemaLocation' => 'http://pkp.sfu.ca native.xsd',
-            'date_submitted' => $this->formatDate($this->paper->getDateSubmitted()),
-            'status' => 3,
-            'submission_progress' => 0,
-            'current_publication_id' => $this->paper->getId(),
-            'stage' => 'production'
-        ]);
-        $articleNode->setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
-
-        /** @var PaperFileDAO */
-        $paperFileDao = DAORegistry::getDAO('PaperFileDAO');
-
-        // Submission Files
-        /** @var PaperFile */
-        foreach ($paperFileDao->getPaperFilesByPaper($this->paper->getId()) as $paperFile) {
-            if ($submissionFileNode = $this->processSubmissionFile($paperFile)) {
-                $articleNode->appendChild($submissionFileNode);
-            }
-        }
-
-        $this->processPublication($articleNode);
-
-        $this->createChildWithText($articleNode, 'pages', $this->paper->getPages(), false);
+        $this->processArticle();
 
         return $this->document->saveXML();
     }
 
     /**
+     * Processes the author
      * @param Author $author
+     * @return DOMElement
      */
-    private function processAuthor($author, $seq) {
+    protected function processAuthor($author, $seq)
+    {
         $authorNode = $this->document->createElement('author');
         
         if ($author->getPrimaryContact()) {
@@ -81,30 +60,12 @@ class Stable321Writer extends BaseXmlWriter {
     /**
      * Process submission_file node
      * @param PaperFile $paperFile
+     * @return DOMDocumentFragment
      */
-    private function processSubmissionFile($paperFile) {
-        static $genreMap;
-
-        if (!$genreMap) {
-            AppLocale::requireComponents([LOCALE_COMPONENT_OCS_AUTHOR]);
-            foreach ([
-                'author.submit.suppFile.researchInstrument' => '{[#GENRE_NAME_RESEARCHINSTRUMENT#]}',
-                'author.submit.suppFile.researchMaterials' => '{[#GENRE_NAME_RESEARCHMATERIALS#]}',
-                'author.submit.suppFile.researchResults' => '{[#GENRE_NAME_RESEARCHRESULTS#]}',
-                'author.submit.suppFile.transcripts' => '{[#GENRE_NAME_TRANSCRIPTS#]}',
-                'author.submit.suppFile.dataAnalysis' => '{[#GENRE_NAME_DATAANALYSIS#]}',
-                'author.submit.suppFile.dataSet' => '{[#GENRE_NAME_DATASET#]}',
-                'author.submit.suppFile.sourceText' => '{[#GENRE_NAME_SOURCETEXTS#]}',
-            ] as $sourceGenre => $targetGenre) {
-                $genreMap[__($sourceGenre)] = $targetGenre;
-            }
-            $genreMap += [
-                'OTHER' => '{[#GENRE_NAME_OTHER#]}',
-                'SUBMISSION' => '{[#GENRE_NAME_SUBMISSION#]}',
-                'STYLE' => '{[#GENRE_NAME_STYLE#]}',
-                'IMAGE' => '{[#GENRE_NAME_IMAGE#]}'
-            ];
-        }
+    protected function processSubmissionFile($paperFile)
+    {
+        $documentFragment = $this->document->createDocumentFragment();
+        $genreMap = $this->getGenreMap();
 
         /** @var PaperGalley|PaperHTMLGalley|SuppFile */
         $ownerGalley = null;
@@ -124,7 +85,7 @@ class Stable321Writer extends BaseXmlWriter {
             }
         }
 
-        $submissionFileNode = $this->document->createElement('submission_file');
+        $submissionFileNode = $documentFragment->appendChild($this->document->createElement('submission_file'));
         $this->setAttributes($submissionFileNode, ['stage' => 'submission', 'id' => $paperFile->getFileId()]);
 
         import('file.PaperFileManager');
@@ -173,11 +134,8 @@ class Stable321Writer extends BaseXmlWriter {
                 $submissionFileNode->appendChild($this->document->createElement('language', $locale));
             }
         } elseif ($ownerGalley instanceof PaperHTMLGalley) {
-            $submissionFileNodes = $this->document->createDocumentFragment();
-            $submissionFileNodes->appendChild($submissionFileNode);
-
             foreach (array_merge([$ownerGalley->getStyleFile()], $ownerGalley->getImageFiles()) as $dependentPaperFile) {
-                $submissionFileNode = $submissionFileNodes->appendChild($this->document->createElement('submission_file'));
+                $submissionFileNode = $documentFragment->appendChild($this->document->createElement('submission_file'));
                 $this->setAttributes($submissionFileNode, ['stage' => 'dependent', 'id' => $dependentPaperFile->getFileId()]);
                 
                 $paperFileManager->readFile($dependentPaperFile->getFileId());
@@ -192,40 +150,52 @@ class Stable321Writer extends BaseXmlWriter {
                     'filesize' => $dependentPaperFile->getFileSize(),
                     'filetype' => $dependentPaperFile->getFileType()
                 ]);
-        
+
                 $this->createLocalizedNodes($revisionNode, 'name', [$this->locale => $dependentPaperFile->getOriginalFileName()]);
 
                 $fileRefNode = $revisionNode->appendChild($this->document->createElement('submission_file_ref'));
                 $fileRefNode->setAttribute('id', $paperFile->getFileId());
                 $fileRefNode->setAttribute('revision', $paperFile->getRevision());
-        
+
                 $embedNode = $revisionNode->appendChild($this->document->createElement('embed', base64_encode($paperFileManager->readFile($dependentPaperFile->getFileId()))));
                 $embedNode->setAttribute('encoding', 'base64');
             }
-            return $submissionFileNodes;
         }
-        return $submissionFileNode;
+        return $documentFragment;
     }
 
     /**
      * Process the issue_identification
      * @param DOMElement $publicationNode
+     * @return DOMElement
      */
-    private function processIssue($publicationNode)
+    protected function processIssue($publicationNode)
     {
         // Issue
         $issueNode = $publicationNode->appendChild($this->document->createElement('issue_identification'));
         $this->createChildWithText($issueNode, 'volume', '{[#ISSUE_VOLUME#]}', false);
         $this->createChildWithText($issueNode, 'number', '{[#ISSUE_NUMBER#]}', false);
         $this->createChildWithText($issueNode, 'year', '{[#ISSUE_YEAR#]}', false);
+        return $issueNode;
+    }
+
+    /**
+     * Retrieves the tag name to be used for the galley
+     * @param PaperGalley|SuppFile $galley
+     * @return string
+     */
+    protected function getGalleyTagName($galley)
+    {
+        return $galley instanceof PaperGalley ? 'article_galley' : 'supplementary_file';
     }
 
     /**
      * Process the article_galley and supplementary_file
-     * @param DOMElement $publicationNode
+     * @return DOMDocumentFragment
      */
-    private function processGalleys($publicationNode)
+    protected function processGalleys()
     {
+        $documentFragment = $this->document->createDocumentFragment();
         /** @var PaperFileDAO */
         $paperFileDao = DAORegistry::getDAO('PaperFileDAO');
 
@@ -238,16 +208,17 @@ class Stable321Writer extends BaseXmlWriter {
             }
 
             if ($galley instanceof PaperGalley) {
-                $locale = $galley->getLocale();
-                $names = [$locale => $galley->getLabel()];
-                $tagName = 'article_galley';
+                $locale = $galley->getLocale() ?: $this->locale;
+                $names = [$locale => $galley->getLabel() ?: strtoupper(pathinfo($paperFile->getFileName(), PATHINFO_EXTENSION))];
             } else {
                 $locale = $this->locale;
                 $names = $galley->getTitle();
-                $tagName = 'supplementary_file';
+                if (!count($names)) {
+                    $names = [$locale => strtoupper(pathinfo($paperFile->getFileName(), PATHINFO_EXTENSION))];
+                }
             }
 
-            $articleGalleyNode = $publicationNode->appendChild($this->document->createElement($tagName));
+            $articleGalleyNode = $documentFragment->appendChild($this->document->createElement($this->getGalleyTagName($galley)));
             $this->setAttributes($articleGalleyNode, ['locale' => $locale, 'approved' => 'true']);
             $idNode = $this->createChildWithText($articleGalleyNode, 'id', $galley->getId(), false);
             $this->setAttributes($idNode, ['type' => 'internal', 'advice' => 'ignore']);
@@ -256,6 +227,7 @@ class Stable321Writer extends BaseXmlWriter {
             $fileRefNode = $articleGalleyNode->appendChild($this->document->createElement('submission_file_ref'));
             $this->setAttributes($fileRefNode, ['id' => $paperFile->getFileId(), 'revision' => $paperFile->getRevision()]);
         }
+        return $documentFragment;
     }
 
     /**
@@ -264,8 +236,9 @@ class Stable321Writer extends BaseXmlWriter {
      * @param array $keywords
      * @param string $keywordsNodeName
      * @param string $keywordNodeName
+     * @return ?DOMElement
      */
-    private function processKeywords($publicationNode, $keywords, $keywordsNodeName, $keywordNodeName)
+    protected function processKeywords($publicationNode, $keywords, $keywordsNodeName, $keywordNodeName)
     {
         foreach (is_array($keywords) ? $keywords : [] as $locale => $keyword) {
             $keywords = array_filter(array_map('trim', preg_split('/[,;]/', $keyword)), 'strlen');
@@ -275,6 +248,7 @@ class Stable321Writer extends BaseXmlWriter {
                 foreach ($keywords as $keyword) {
                     $this->createChildWithText($keywordsNode, $keywordNodeName, $keyword, false);
                 }
+                return $keywordsNode;
             }
         }
     }
@@ -282,8 +256,9 @@ class Stable321Writer extends BaseXmlWriter {
     /**
      * Process the authors node
      * @param DOMElement $publicationNode
+     * @return ?DOMElement
      */
-    private function processAuthors($publicationNode)
+    protected function processAuthors($publicationNode)
     {
         if (!count($this->paper->getAuthors())) {
             return;
@@ -293,14 +268,16 @@ class Stable321Writer extends BaseXmlWriter {
             $authorNode = $this->processAuthor($author, $i + 1);
             $authorsNode->appendChild($authorNode);
         }
+        return $authorsNode;
     }
 
 
     /**
      * Process the publication node
      * @param DOMElement $articleNode
+     * @return DOMElement
      */
-    private function processPublication($articleNode)
+    protected function processPublication($articleNode)
     {
         // Publication
         /** @var DOMElement */
@@ -353,7 +330,74 @@ class Stable321Writer extends BaseXmlWriter {
 
         $this->processAuthors($publicationNode);
 
-        $this->processGalleys($publicationNode);
+        $publicationNode->appendChild($this->processGalleys());
         $this->processIssue($publicationNode);
+        return $publicationNode;
+    }
+
+    /**
+     * Process the article node
+     * @return DOMElement
+     */
+    protected function processArticle()
+    {
+        // Article
+        $articleNode = $this->document->appendChild($this->document->createElementNS('http://pkp.sfu.ca', 'article'));
+        $this->setAttributes($articleNode, [
+            'xsi:schemaLocation' => 'http://pkp.sfu.ca native.xsd',
+            'date_submitted' => $this->formatDate($this->paper->getDateSubmitted()),
+            'status' => 3,
+            'submission_progress' => 0,
+            'current_publication_id' => $this->paper->getId(),
+            'stage' => 'production'
+        ]);
+        $articleNode->setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+
+        /** @var PaperFileDAO */
+        $paperFileDao = DAORegistry::getDAO('PaperFileDAO');
+
+        // Submission Files
+        /** @var PaperFile */
+        foreach ($paperFileDao->getPaperFilesByPaper($this->paper->getId()) as $paperFile) {
+            if ($submissionFileNode = $this->processSubmissionFile($paperFile)) {
+                $articleNode->appendChild($submissionFileNode);
+            }
+        }
+
+        $this->processPublication($articleNode);
+        $this->createChildWithText($articleNode, 'pages', $this->paper->getPages(), false);
+
+        return $articleNode;
+    }
+
+    /**
+     * Builds the genre map
+     */
+    protected function getGenreMap()
+    {
+        static $genreMap;
+
+        if (!$genreMap) {
+            AppLocale::requireComponents([LOCALE_COMPONENT_OCS_AUTHOR]);
+            foreach ([
+                'author.submit.suppFile.researchInstrument' => '{[#GENRE_NAME_RESEARCHINSTRUMENT#]}',
+                'author.submit.suppFile.researchMaterials' => '{[#GENRE_NAME_RESEARCHMATERIALS#]}',
+                'author.submit.suppFile.researchResults' => '{[#GENRE_NAME_RESEARCHRESULTS#]}',
+                'author.submit.suppFile.transcripts' => '{[#GENRE_NAME_TRANSCRIPTS#]}',
+                'author.submit.suppFile.dataAnalysis' => '{[#GENRE_NAME_DATAANALYSIS#]}',
+                'author.submit.suppFile.dataSet' => '{[#GENRE_NAME_DATASET#]}',
+                'author.submit.suppFile.sourceText' => '{[#GENRE_NAME_SOURCETEXTS#]}',
+            ] as $sourceGenre => $targetGenre) {
+                $genreMap[__($sourceGenre)] = $targetGenre;
+            }
+            $genreMap += [
+                'OTHER' => '{[#GENRE_NAME_OTHER#]}',
+                'SUBMISSION' => '{[#GENRE_NAME_SUBMISSION#]}',
+                'STYLE' => '{[#GENRE_NAME_STYLE#]}',
+                'IMAGE' => '{[#GENRE_NAME_IMAGE#]}'
+            ];
+        }
+
+        return $genreMap;
     }
 }
